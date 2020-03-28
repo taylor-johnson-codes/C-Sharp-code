ce1471c61040c05aa5b46641ecb0c5f0e3f0b3f3
Taylor Johnson <60452595+taylor-johnson-codes@users.noreply.github.com>
2020-03-28 15:56:28 -0700

    Add files via upload

---
+++ ArraysMultidimensional.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ArraysMultidimensional
+{
+    class ArraysMultidimensional
+    {
+        static void Main(string[] args)
+        {
+
+            // EXAMPLE 1
+
+            // creating a new two-dimensional (2D) array
+            // it's a 3x2 array meaning it has three separate arrays each containing two elements
+            int[,] numbers2D = new int[3, 2] { { 22, 33 }, { 44, 55 }, { 66, 77 } };
+
+            for (int i = 0; i < 3; i++)  // iterating through each of the three arrays
+            {
+                for (int d = 0; d < 2; d++)  // iterating through each of the elements in the array
+                {
+                    Console.WriteLine(numbers2D[i, d]);  // outputs each value of each index position of each of the three arrays
+                }
+            }
+
+
+            // EXAMPLE 2
+
+            // creating a new three-dimensional (3D) array
+            // it's a 3x3 array meaning it has three separate arrays each containing three elements
+            int[,] numbers3D = new int[3, 3] { { 11, 22, 33 }, { 44, 55, 66 }, { 77, 88, 99 } };
+
+            for (int a = 0; a < 3; a++)  // iterating through each of the three arrays
+            {
+                for (int b = 0; b < 3; b++)  // iterating through each of the elements in the array
+                {
+                    Console.WriteLine("numbers3D [{0},{1}] = {2}", a, b, numbers3D[a, b]);
+                    // outputs the name of the array and the value of each index position in the array
+                }
+            }
+            Console.ReadLine();
+
+
+            /*
+            REFERENCES:
+            https://www.tutorialspoint.com/csharp/csharp_multi_dimensional_arrays.htm
+            https://www.tutorialsteacher.com/csharp/csharp-class
+            */
+        }
+    }
+}
---
+++ ClassSimple.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ClassSimple
+{
+    class ClassSimple
+    {
+        static void Main(string[] args)
+        {
+
+            TaylorHasClass TJ = new TaylorHasClass();  // creating a new instance (called TJ) of the TaylorHasClass object
+            TJ.Right();  // calling the Right method from TaylorHasClass
+            TJ.Wrong();  // calling the Wrong method from TaylorHasClass
+
+            Addition Adding = new Addition();  // creating a new instance (called Adding) of the Addition object
+            Console.WriteLine(Adding.Add2(5, 10));  // calling and printing the Add2 method with two test parameters
+            Console.ReadLine();
+        }
+    }
+    class TaylorHasClass
+    // creating a class called TaylorHasClass
+    {
+        public void Right()  // creating a method called Right
+        {
+            Console.WriteLine("You are right!");
+            // when the Right method is called, "You are right!" will be printed
+        }
+        public void Wrong()  // creating a method called Wrong
+        {
+            Console.WriteLine("You are wrong!");
+            // when the Wrong method is called, "You are wrong!" will be printed
+        }
+
+        /*
+         * Note:
+         * The methods are performing an action which is not the same as returning something.
+         * Therefore, no return statement is used and the return type of the method before the method's name is void.
+         */
+
+    }
+    class Addition
+    // creating a class called Addition
+    {
+        public int Add2(int a, int b)  // creating a new method that will add two integer parameters together
+        {
+            return a + b;  // method will return the sum of the two integer parameters
+        }
+
+        /*
+         * Note:
+         * The method is returning an int data type.
+         * Therefore, a return statement is used in the body and the return type of the method before the method's name is int.
+         */
+    }
+}
